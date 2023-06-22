45c18cd1b46db6fced59172b0497abcca6a44ca6
marianaalvarezr17 <mariana.mar170906@gmail.com>
2023-06-21 19:56:37 -0500

    Utilizamos Java por primera vez

--- app/src/main/java/com/example/puebliandoapp/MainActivity.java
+++ app/src/main/java/com/example/puebliandoapp/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.puebliandoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    //ATRIBUTOS= Elementos de la actividad
+
+    MediaPlayer cancion;
+
+    //METODOS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cancion =MediaPlayer.create(this,R.raw.audioapp);
+        cancion.start();
+
+        //Temporizamos el Splash
+        TimerTask inicioAPP=new TimerTask() {
+            @Override
+            public void run() {
+                //Lanzo el HOME
+                Intent lanzamiento=new Intent(MainActivity.this,Home.class);
+                startActivity(lanzamiento);
+
+
+            }
+        };
+        Timer tiempo=new Timer();
+        tiempo.schedule(inicioAPP,10000);
     }
 }
